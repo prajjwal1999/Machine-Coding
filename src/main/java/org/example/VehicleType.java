11067882ca811a08ecc6fd42af6f1401069c2d36
prajjwal <prajjwal875@gmail.com>
2025-02-05 13:57:32 +0530

    added plan

--- src/main/java/org/example/VehicleType.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-enum VehicleType {
-    CAR, BIKE, TRUCK
-}
--- src/main/java/splitwise/Main.java
+++
@@ -1,9 +0,0 @@
-package splitwise;
-import java.util.*;
-public class Main {
-    private static Users users;
-    public static void main(String[] args) {
-        System.out.println("Welcome to Splitwise app..");
-
-    }
-}
